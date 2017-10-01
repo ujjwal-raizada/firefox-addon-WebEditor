5f4c075dedc23125bce858f1ebb6c505334173f8
ujjwalrox <ujjwalraizada@gmail.com>
2017-10-01 08:01:20 +0530

    First commit to firefox addon , for making any web page edittable

---
+++ background.js
@@ -0,0 +1,26 @@
+function openPage() {
+ 
+  //script inject code start
+
+  function onExecuted(result) {
+    console.log(`Editor start`);
+  }
+  //handles error
+  function onError(error) {
+    console.log(`Error: ${error}`);
+  }
+  //inject the following
+  //javascript in the current active
+  //tab
+
+  var editorcode = "alert('Website is in Editor mode !! After editting , use CTRL + S , to save !');document.body.contentEditable = 'true'; document.designMode = 'on'; void 0";
+  
+  var executing = browser.tabs.executeScript({
+    code: editorcode
+  });
+  executing.then(onExecuted, onError);
+//script inject code ends
+
+}
+
+browser.browserAction.onClicked.addListener(openPage);
